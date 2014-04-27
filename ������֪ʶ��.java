5acdde8490ed81d4f6236a81dbe7139f722f06be
Smarking <smarkingzhao@163.com>
2014-04-28 01:46:09 +0800

    添加 知识点、认识

---
+++ ������֪ʶ��.java
@@ -0,0 +1,72 @@
+知识点：功能点
+
+熟练掌握面向对象思想以及常见的设计模式。
+熟练使用集合、IO流、多线程、反射以及动态代理机制等。
+		多线程断点下载/工厂模式的实现/面向切面编程AOP
+熟悉Android动画、选择器、样式、主题以及常用布局的使用。
+熟练掌握Android四大组件。
+		程序密码锁
+熟悉自定义控件，触摸事件处理及分发机制
+		自定义View实现滑动Button，自定义ViewGroup实现ViewPager ，ScrollView与ListView嵌套
+熟悉Android下的Handler消息处理机制以及AsyncTask执行原理。
+熟悉XML/JSON/SQLite数据处理。
+熟悉掌握ListView的优化。
+熟悉异步任务加载网络图片的三级缓存技术（LruCache、SDCard、Internet）
+熟悉Androidd的JNI实现JAVA与C/C++程序间的调用及回调。
+熟悉Fragment+ViewPager混合使用以及生命周期。
+熟悉Android框架搭建
+		统一界面管理、协议通用化封装、注解实现数据库操作通用化
+
+了解读源码了解Android整个启动过程。
+了解Activity、Window和View三者间的关系。
+了解Android系统下消息推送机制，Socket长连接。
+了解嵌入式硬件以及智能家居Zigbee技术。
+
+
+
+实习经历
+	职责
+
+
+
+
+
+没有app，拿纸的理由，为了更好的展现 知识点，而特意策划的方案
+硬件是软件的基石，相符想成的，优化，内存128G，flag位 ，在USB和耳机口可以做外扩功能，PCB对全局的把控能力，做事的方针，对组织活动，在一大帮android工程师面前讲解智能穿戴的原理。何况android的基础硬件就是嵌入式设备。吹裙子应用，手机是如何感知的。
+在大学的生活是多么的充实。
+
+策划、执行、反馈、总结。
+大赛时光
+大小公司认识的转变
+自己的优缺点
+平台对自己的重要性
+创意的源泉
+自己的毕设
+
+
+
+
+
+
+注解
+findviewbyid
+对数据库操作通用化的实现。
+黑名单反射获取manager拦截电话。
+Service动态注册广播/
+manager
+
+handler
+asynctask
+listview
+
+小小的我在有我大大的天
+
+对于代码库的作用。
+
+
+
+13938221354
+
+
+
+@问题汇总
